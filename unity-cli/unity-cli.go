8b66367b6a672c7c807911517432a6b3debc4fce
Igor <idyurev@gmail.com>
2019-01-14 22:51:37 +0300

    CLI tool v0.1

--- unity-cli/unity-cli.go
+++ unity-cli/unity-cli.go
@@ -7,41 +7,44 @@ import (
 	"log"
 )
 
-var BaseUrl string = "192.168.130.87"
-var SecureConn bool = true
-
-//////////////////////////////
-var poolId string = "main"
-
-//"pool_1"
-var nasId string = "nas-001"
-
-//"nas_1"
-var root_access_hosts = "test_ocp_cluster"
-
-//"Subnet_6"
-var demoPVName string = "demo-pv"
-
-var RestUser string = "admin"
-var RestPassw = "Qwe12345!"
+func emthyArg(arg *string) bool {
+	if *arg == "" {
+		return true
+	}
+	return false
+}
 
 func main() {
 	base_url := flag.String("--base_url", "", "Unity REST API Base URL")
 	username := flag.String("--username", "", "Unity REST API Username")
 	password := flag.String("--password", "", "Unity REST API Password")
-	pool_name := ""
-	nas_name := ""
-	vol_name := ""
-	access_hosts := ""
+	pool_name := flag.String("--pool-name", "", "Storage pool name")
+	nas_name := flag.String("--nas-name", "", "NAS name")
+	vol_name := flag.String("--volume-name", "", "Volume name")
+	access_hosts := flag.String("--access-hosts", "", "Unity access hosts")
+	vol_size := flag.Int("--volume-size", 0, "Volume size in Gigabytes")
+	create_nfs_volume := flag.Bool("--create-nfs-volume", true, "Create Volume with NFS share")
+	flag.Parse()
 
-	test_unity, err := unity_api.NewUnityDataStore("192.168.130.87", "admin", "Qwe12345!")
-	if err != nil {
-		log.Fatal(err)
+	if !emthyArg(base_url) && !emthyArg(username) && !emthyArg(password) {
+		unity_ds, err := unity_api.NewUnityDataStore(*base_url, *username, *password)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if *create_nfs_volume {
+			if !emthyArg(pool_name) && !emthyArg(nas_name) && !emthyArg(vol_name) && !emthyArg(access_hosts) && *vol_size != 0 {
+				unity_access_hosts := []string{}
+				res, create_err := unity_ds.CreateFSwithNFSExport(*vol_name, *pool_name, *nas_name, "", unity_access_hosts, unity_api.Gb_to_Bytes(*vol_size))
+				if create_err != nil {
+					log.Fatal(create_err)
+				}
+				fmt.Printf("Create volume with NFS share, name: %%d, volume name: %s\n", vol_size, vol_name)
+				fmt.Printf("Rest API responce: %s\n", res.RequestData)
+			}
+			log.Fatal("You must give arguments: --pool-name, --nas-name,  --volume-name, --volume-size, --access-hosts !!!")
+		}
+		log.Fatal("You must set least one action, --create-nfs-volume for example!!!")
 	}
-	fmt.Printf("%s\n", "Create FS + NFS share.")
-	resp, new_vol_err := test_unity.CreateFSwithNFSExport(pv_name, poolId, nasId, "", access_hosts, unity_api.Gb_to_Bytes(pv_size))
-	if new_vol_err != nil {
-		log.Printf("%s\n", new_vol_err)
-	}
-	fmt.Printf("Response from Unity API: %s\n", resp)
+	log.Fatal("You must give credentials for REST API!!")
+
 }
