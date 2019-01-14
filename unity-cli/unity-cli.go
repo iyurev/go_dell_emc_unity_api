dbc64f32b0e1df5582a98eee4636619b5cdd1435
Igor <idyurev@gmail.com>
2019-01-14 16:06:12 +0300

    Some refactoring #01 Use names for Pool and NasServer instead of IDs

--- unity-cli/unity-cli.go
+++ unity-cli/unity-cli.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/iyurev/go_dell_emc_unity_api/unity_api"
 	"log"
@@ -19,22 +20,28 @@ var nasId string = "nas-001"
 var root_access_hosts = "test_ocp_cluster"
 
 //"Subnet_6"
-var demoPVName string = "ocp_pv_02"
+var demoPVName string = "demo-pv"
 
 var RestUser string = "admin"
 var RestPassw = "Qwe12345!"
 
 func main() {
+	base_url := flag.String("--base_url", "", "Unity REST API Base URL")
+	username := flag.String("--username", "", "Unity REST API Username")
+	password := flag.String("--password", "", "Unity REST API Password")
+	pool_name := ""
+	nas_name := ""
+	vol_name := ""
+	access_hosts := ""
+
 	test_unity, err := unity_api.NewUnityDataStore("192.168.130.87", "admin", "Qwe12345!")
 	if err != nil {
 		log.Fatal(err)
 	}
-	access_hosts := []string{root_access_hosts, "prod_ocp_cluster"}
-
-	resp, new_vol_err := test_unity.CreateFSwithNFSExport(demoPVName, poolId, nasId, "", access_hosts, unity_api.Gb_to_Bytes(10))
+	fmt.Printf("%s\n", "Create FS + NFS share.")
+	resp, new_vol_err := test_unity.CreateFSwithNFSExport(pv_name, poolId, nasId, "", access_hosts, unity_api.Gb_to_Bytes(pv_size))
 	if new_vol_err != nil {
-		log.Fatal(new_vol_err)
+		log.Printf("%s\n", new_vol_err)
 	}
-	fmt.Printf("%s      %s\n", resp.RequestData, resp.RespData)
-
+	fmt.Printf("Response from Unity API: %s\n", resp)
 }
